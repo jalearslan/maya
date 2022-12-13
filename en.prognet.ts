3edf40d34be429e3e2f84c31aa42db00534735e8
jalearslan <jaleearslann@gmail.com>
2022-12-13 15:49:08 +0300

    Delete en.prognet.ts

--- en.prognet.ts
+++
@@ -1,6 +0,0 @@
-{
-    "cannot_buy_unit": {
-        "not_enough_money": "You tried to buy or upgrade an unit, but you have only  gold, and an unit costs .",
-        "selection_not_empty": "You tried to buy a new unit, but you have a tower in your selection. Place your tower first."
-    }
-}
